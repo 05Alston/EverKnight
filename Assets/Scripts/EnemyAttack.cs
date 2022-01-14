ee37b21d0c5d1ced2c026d68ea85513f93c95c86
Shubham Talawadekar <shubhamtalawadekar2002@gmail.com>
2022-01-14 22:41:19 +0530

    Switch Levels

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class EnemyAttack : MonoBehaviour
+{
+    private float enemy =0;
+    public GameObject game;
+    public GameObject changeScene;
+    public TextMeshProUGUI count;
+    public void OnTriggerEnter2D(Collider2D o){
+        if(o.transform.tag == "Enemy"){
+            enemy++;
+            count.text=enemy.ToString();
+            Destroy(o.gameObject);
+        }
+        if(enemy==10){
+            game.SetActive(false);
+            changeScene.SetActive(true);
+        }
+    }
+
+}
