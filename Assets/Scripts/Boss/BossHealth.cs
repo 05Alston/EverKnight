40c6b93fe20d861f14855cbb076361e268bfac03
Shubham Talawadekar <shubhamtalawadekar2002@gmail.com>
2022-02-09 20:19:18 +0530

    boss

---
+++ Assets/Scripts/Boss/BossHealth.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealth : MonoBehaviour
+{
+
+    private Animator animator;
+    public float maxHealth = 10;
+    public float currentHealth;
+    [SerializeField] public BossHealthBar healthBar;// Healthbar will have value between 0 and 1
+
+
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        currentHealth = maxHealth;
+        healthBar.SetHealth(maxHealth / maxHealth);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        gameObject.GetComponent<EnemyFollow>().delay = 0;
+        currentHealth -= damage;
+        if (currentHealth <=0)
+        {
+            Die();
+            EnemyCount.instance.ChangeCount();
+        }
+        healthBar.SetHealth(currentHealth / maxHealth);
+
+        // TODO: Play hurt animation
+        // Move enemy back on hit
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x + 0.8f, gameObject.transform.position.y, gameObject.transform.position.z);
+    }
+
+    void Die()
+    {
+        // Disable enemy
+        gameObject.layer = 3;
+        gameObject.GetComponent<EnemyFollow>().enabled = false;
+        gameObject.GetComponent<EnemyAttack1>().enabled = false;
+        // Play die animation
+        animator.SetBool("isDead", true);
+        gameObject.SetActive(false);
+    }
+}
